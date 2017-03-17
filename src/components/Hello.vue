5d94182482189c36a65edeeb8a6487d499d3bc2e
ETHAN ANDERSON (CRGL-THIRDPARTY.COM) <ETHAN_ANDERSON@CRGL-THIRDPARTY.COM>
2017-03-17 12:56:30 -0500

    Init

---
+++ src/components/Hello.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+  div
+    h1(class="hello") sup?
+    h2 we in here
+    button(class="button")
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+h1, h2
+  font-weight: normal
+
+ul
+  list-style-type: none
+  padding: 0
+
+li
+  display: inline-block
+  margin: 0 10px
+
+a
+  color: #42b983
+</style>
